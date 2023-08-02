3f793ff1284ebfe2f6316b9e23ec76cbd9769559
Tenno <themenos1201@gmail.com>
2023-08-02 08:49:03 +0600

    Scene transition prefab added

--- Assets/Scripts/SceneTransitor/SceneTransition.cs
+++ Assets/Scripts/SceneTransitor/SceneTransition.cs
@@ -7,19 +7,20 @@ public class SceneTransition : MonoBehaviour
     [SerializeField] private Animator _animator;
 
     private Coroutine _sceneLoadCoroutine;
-    private readonly int _startTransition = Animator.StringToHash("Start");
-    private readonly int _endTransition = Animator.StringToHash("End");
+    private readonly int _crossFadeStart = Animator.StringToHash("Start");
+    private readonly int _crossFadeEnd = Animator.StringToHash("End");
     private readonly WaitForSeconds _waitForSecond = new(1);
+    private AsyncOperation _sceneLoadOperation;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
 
-    public void LoadScene(string scene)
+    public void LoadScene(string scene, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
     {
         StopLoadScene();
-        _sceneLoadCoroutine = StartCoroutine(TransitScene(scene));
+        _sceneLoadCoroutine = StartCoroutine(TransitScene(scene, loadSceneMode));
     }
 
     private void StopLoadScene()
@@ -27,16 +28,21 @@ public class SceneTransition : MonoBehaviour
         if (_sceneLoadCoroutine != null) StopCoroutine(_sceneLoadCoroutine);
     }
 
-    private IEnumerator TransitScene(string scene)
+    private IEnumerator TransitScene(string scene, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
     {
         _animator.enabled = true;
 
-        _animator.SetTrigger(_endTransition);
+        _animator.SetTrigger(_crossFadeEnd);
         
         yield return _waitForSecond;
 
-        SceneManager.LoadSceneAsync(scene);
+        _sceneLoadOperation = SceneManager.LoadSceneAsync(scene, loadSceneMode);
+        _sceneLoadOperation.completed += CrossFadeStart;
+    }
 
-        _animator.SetTrigger(_startTransition);
+    private void CrossFadeStart(AsyncOperation operation)
+    {
+        _animator.SetTrigger(_crossFadeStart);
+        _sceneLoadOperation.completed -= CrossFadeStart;
     }
 }
